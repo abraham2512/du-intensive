de093de863172b74a484f83541a7a2104d4e82ae
abraham2512 <abraham96miller@gmail.com>
2024-08-06 14:35:49 -0400

    changed to quay

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export FEDORA_STRESSNG_TEST_IMAGE=ghcr.io/abraham2512/fedora-stress-ng:master
-export CURL_TEST_IMAGE=quay.io/cloud-bulldozer/curl:latest
-export NGINX_TEST_IMAGE=quay.io/cloud-bulldozer/sampleapp:latest
-export KUBECTL_TEST_IMAGE=bitnami/kubectl:latest
+export FEDORA_STRESSNG_TEST_IMAGE=quay.io/rh_ee_apalanis/fedora-stress-ng
+export CURL_TEST_IMAGE=quay.io/rh_ee_apalanis/curl
+export NGINX_TEST_IMAGE=quay.io/rh_ee_apalanis/sampleapp
+export KUBECTL_TEST_IMAGE=quay.io/rh_ee_apalanis/kubectl
 kube-burner init --config du-intensive.yml
