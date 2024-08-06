9d6b0e4a19ff76418309db81ce5bc6988b3f259d
abraham2512 <abraham96miller@gmail.com>
2024-08-06 15:45:12 -0400

    registry var in template

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export FEDORA_STRESSNG_TEST_IMAGE=quay.io/rh_ee_apalanis/fedora-stress-ng
-export CURL_TEST_IMAGE=quay.io/rh_ee_apalanis/curl
-export NGINX_TEST_IMAGE=quay.io/rh_ee_apalanis/sampleapp
-export KUBECTL_TEST_IMAGE=quay.io/rh_ee_apalanis/kubectl
+export REGISTRY=quay.io/rh_ee_apalanis
 kube-burner init --config du-intensive.yml
