0bb46658ff7cf3d87edb22993c62c5256a288152
abraham2512 <abraham96miller@gmail.com>
2024-08-07 13:10:15 -0400

    updates

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export FEDORA_STRESSNG_TEST_IMAGE=ghcr.io/abraham2512/fedora-stress-ng:master
-export CURL_TEST_IMAGE=quay.io/cloud-bulldozer/curl:latest
-export NGINX_TEST_IMAGE=quay.io/cloud-bulldozer/sampleapp:latest
-export KUBECTL_TEST_IMAGE=bitnami/kubectl:latest
+export REGISTRY=quay.io/rh_ee_apalanis
 kube-burner init --config du-intensive.yml
